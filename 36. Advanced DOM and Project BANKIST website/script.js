0d87ad758b47aad7d43866495412bc64df91052c
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-02 09:25:43 +0200

    Update script.js

--- 36. Advanced DOM and Project BANKIST website/script.js
+++ 36. Advanced DOM and Project BANKIST website/script.js
@@ -44,3 +44,20 @@ const allButtons = document.getElementsByTagName('button')
 console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'))
+/Creating and inserting elements
+
+// .insertAdjacentHTML
+
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+message.textContent = 'We use cookies for improved functionality and analytics'
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it</button>'
+// header.prepend(message)
+// header.append(message.cloneNode(true))
+
+header.after(message)
+//header.before(message.cloneNode(true))
+//header.before(message.cloneNode(true))
+
+//Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click',()=> message.remove())
