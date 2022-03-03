bb006f1e568213358235ae8a5867c55c4a1b783d
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-03 16:50:54 +0200

    Update script.js

--- 36. Advanced DOM and Project BANKIST website/script.js
+++ 36. Advanced DOM and Project BANKIST website/script.js
@@ -76,3 +76,15 @@ console.log(getComputedStyle(message).height);
 
 document.documentElement.style.setProperty('--color-primary', 'orangered')
 
+///Attributes
+
+const logo = document.querySelector('.nav__logo')
+console.log(logo);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+console.log(logo.getAttribute('designer'));
+console.log(logo.getAttribute('name'));
+logo.setAttribute('company', 'Bankist')
+console.log(logo.getAttribute('src'));
+
