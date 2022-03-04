bea7a1a04a7ae940d94f2be8c398d5d847ae8528
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-04 15:45:26 +0200

    Update script.js

--- 36. Advanced DOM and Project BANKIST website/script.js
+++ 36. Advanced DOM and Project BANKIST website/script.js
@@ -106,4 +106,6 @@ const alertH1 = function(){
 
 const h1 =document.querySelector('h1')
 h1.addEventListener('mouseenter', alertH1 )
-
+setTimeout(() => {
+  h1.removeEventListener('mouseenter',alertH1 )
+}, 2000);
