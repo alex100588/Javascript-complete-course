bb93ba576a77fbd81b4216eb6f4561b506450c3b
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-08 15:59:56 +0200

    Update script.js

--- 38. OOP/script.js
+++ 38. OOP/script.js
@@ -12,3 +12,9 @@ const Person = function(firstName, birthYear){
     // }
 }
 
+const alex = new Person('Alex', 1998)
+const jack = new Person('Jack', 2002)
+console.log(alex);
+console.log(jack);
+// alex.calcAge(2022)
+// jack.calcAge(2022)
