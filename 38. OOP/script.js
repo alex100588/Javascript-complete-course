459e963850398bb3280637f240e68d9248e9a39e
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-08 15:59:18 +0200

    Add files via upload

---
+++ 38. OOP/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+/////// Constructor functions and the new Operator
+
+const Person = function(firstName, birthYear){
+    this.firstName = firstName
+    this.birthYear = birthYear
+
+    // this.calcAge = function(year){
+    //     console.log(year - this.birthYear );
+    // }
+}
+
