c29f7f6d20c4f2a9f81d90c1017006781d326eb8
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-13 09:42:28 +0200

    Update script.js

--- 38. OOP/script.js
+++ 38. OOP/script.js
@@ -299,3 +299,31 @@ Car.prototype.decelerate = function(){
     this.speed -=10
     return `${this.make} is going with ${this.speed} km/h`
 }
+
+const EV = function(make, speed, charge){
+    Car.call(this, make, speed)
+    this.charge = charge
+}
+
+EV.prototype = Object.create(Car.prototype)
+
+EV.prototype.chargeBattery = function(chargeTo){
+    this.charge = chargeTo
+}
+
+EV.prototype.accelerate = function(){
+    this.speed +=20
+    this.charge --
+    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
+}
+
+const tesla = new EV('Tesla', 120, 23)
+console.log(tesla);
+
+tesla.chargeBattery(80)
+console.log(tesla);
+
+tesla.accelerate()
+tesla.accelerate()
+tesla.accelerate()
+console.log(tesla);
