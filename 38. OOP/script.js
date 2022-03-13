11127177b810aff1d1e402cf8fa902dbced6c404
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-13 09:41:45 +0200

    Update script.js

--- 38. OOP/script.js
+++ 38. OOP/script.js
@@ -270,3 +270,16 @@ console.log(anca);
 
 const adina = new Student3('Math', '10', 'Adina', 2010)
 console.log(adina);
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
