ce71a1dc2df328bead35818a3eb74ce1d8f363e9
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-14 09:41:44 +0200

    Update script.js

--- 38. OOP/script.js
+++ 38. OOP/script.js
@@ -327,3 +327,43 @@
 // tesla.accelerate()
 // tesla.accelerate()
 // console.log(tesla);
+
+
+///////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+      this.fullName = fullName;
+      this.birthYear = birthYear;
+    }
+  
+    calcAge() {
+      return 2022 - this.birthYear
+    }
+  
+    greet() {
+      console.log(`Hey ${this.fullName}`);
+    }
+  
+    get age() {
+      return 2037 - this.birthYear;
+    }
+  
+    set fullName(name) {
+      if (name.includes(' ')) this._fullName = name;
+      else alert(`${name} is not a full name!`);
+    }
+  
+    get fullName() {
+      return this._fullName;
+    }
+  
+    // Static method
+    static hey() {
+      console.log('Hey there 👋');
+    }
+  }
+
+  const alex = new PersonCl("Ionasc Alex", 1990)
+  console.log(alex);
