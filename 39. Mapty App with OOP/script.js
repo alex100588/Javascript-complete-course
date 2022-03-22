36c3b1b65f392d869467118b06e803eba95bfaa9
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-22 13:54:49 +0200

    Update script.js

--- 39. Mapty App with OOP/script.js
+++ 39. Mapty App with OOP/script.js
@@ -98,3 +98,17 @@ class App {
         }
       );
   }
+
+    _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
