af2f1a5f0b53769c1cbc14df1a0d5f28d562b733
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-22 13:55:07 +0200

    Update script.js

--- 39. Mapty App with OOP/script.js
+++ 39. Mapty App with OOP/script.js
@@ -112,3 +112,11 @@ class App {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
+      
+      // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
+  }
