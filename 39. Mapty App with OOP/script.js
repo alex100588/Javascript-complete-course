06002ad436ed0d2e9950dc92e76fa8b7fbb67917
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-22 13:52:58 +0200

    Update script.js

--- 39. Mapty App with OOP/script.js
+++ 39. Mapty App with OOP/script.js
@@ -23,3 +23,14 @@ class Workout {
     this.clicks++;
   }
 }
+
+
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
