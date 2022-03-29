592318cdbd4a2360450e55d5260d7a8bd115bc67
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-29 16:29:58 +0300

    Update script.js

--- 40. Promises/script.js
+++ 40. Promises/script.js
@@ -28,3 +28,31 @@ const renderError = function(msg){
     countriesContainer.insertAdjacentText('beforeend', msg)
     // countriesContainer.style.opacity = 1;
 }
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(res =>{
+        console.log(res);
+        if(!res.ok){
+            throw new Error(`Country not found ${res.status}`)
+        }
+        
+        return res.json()
+    })
+    .then(data => {
+        const second = data[0].borders
+
+        if(!second) return
+
+        renderCountry(data[0])
+        return fetch(`https://restcountries.com/v3.1/name/${country}`)
+    }).then(res => res.json())
+    .then(data=> renderCountry(data[0]))
+    .catch(err => renderError(`Something went wrong ${err.message}. Try again!`))
+    .finally(()=>{
+        countriesContainer.style.opacity = 1;
+    })
+}
+
+btn.addEventListener('click', function(){
+    getCountryData('portugal') 
+})
