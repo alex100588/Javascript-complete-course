4c09bcd39c657211c545470a85ffd0e219c1ffd4
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-03-29 16:29:32 +0300

    Update script.js

--- 40. Promises/script.js
+++ 40. Promises/script.js
@@ -3,3 +3,23 @@
 //////Promises and fetch api
 
 
+const countriesContainer = document.querySelector('.countries')
+const btn = document.querySelector('.btn-country')
+
+const renderCountry = function (data, className = '') {
+    const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.svg}" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${
+            ( +data.population / 1000000).toFixed(1)} milion people</p>
+            <p class="country__row"><span>🗣️</span> ${data.languages.por}</p>
+            <p class="country__row"><span>💰</span>${data.currencies.EUR.name} </p>
+            </div>
+        </article>
+        `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // countriesContainer.style.opacity = 1;
+  };
